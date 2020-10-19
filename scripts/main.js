162217fbea3d32113b68af09e411ce475d5b2395
JOE-DEK-IT <weerapatonline@gmail.com>
2020-10-20 03:34:39 +0700

    add image1-10

--- scripts/main.js
+++ scripts/main.js
@@ -44,10 +44,10 @@ $('a.smooth-scroll')
 
 
 // Get the modal
-var modal = document.getElementById("myModal");
+var modal = document.getElementById("myModal1");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btn = document.getElementById("myBtn1");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -57,9 +57,11 @@ btn.onclick = function() {
   modal.style.display = "block";
 }
 
+
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+  
 }
 
 // When the user clicks anywhere outside of the modal, close it
@@ -67,6 +69,7 @@ window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+
 }
 
 
@@ -130,4 +133,29 @@ else if (document.layers)
 associateimages();
 
 
+// Get the modal
+var modalit = document.getElementById("myModalit");
+
+// Get the button that opens the modal
+var btnit = document.getElementById("myBtnit");
+
+// Get the <span> element that closes the modal
+var spanit = document.getElementsByClassName("closeit")[0];
+
+// When the user clicks on the button, open the modal
+btnit.onclick = function() {
+  modalit.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+spanit.onclick = function() {
+  modalit.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modalit) {
+    modalit.style.display = "none";
+  }
+}
 
